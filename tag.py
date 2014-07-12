8b0c77c5a27d9aecbbbe7c13dde3874412678318
alexesprit <alex.esprit@gmail.com>
2014-07-12 16:31:26 +0400

    Fix TypeError if Python 3 intepreter is using

--- tag.py
+++ tag.py
@@ -69,11 +69,15 @@ class TagWrapper:
         extension = os.path.splitext(filename)[1]
         try:
             tag_type = TagWrapper.WRAPPER_MAP[extension]
-
         except KeyError:
             raise RuntimeError
-
         self.audio = tag_type(filename)
-        self.__getitem__ = self.audio.__getitem__
-        self.__setitem__ = self.audio.__setitem__
-        self.save = self.audio.save
+
+    def __getitem__(self, item):
+        return self.audio[item]
+
+    def __setitem__(self, item, value):
+        self.audio[item] = [value]
+
+    def save(self):
+        self.audio.save()
